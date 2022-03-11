c4d04c93aa1e3aadfa453cdb8551e48524790fc4
Kirils Kozemjakins <78017496+kkozemjakins@users.noreply.github.com>
2022-03-11 08:07:48 +0200

    Update Library.cs

--- Library.cs
+++ Library.cs
@@ -2,13 +2,13 @@ using System;
 
 class Library{
   public string Name;
-  public int WorkHoursStart;
-  public int WorkHoursEnd;
+  public double WorkHoursStart;
+  public double WorkHoursEnd;
   public int NumberBooks;
   public string City;
   public string District;
 
-  public Library(string name, int workHoursStart, int workHoursEnd, int numberBooks, string city, string district){
+  public Library(string name, double workHoursStart, double workHoursEnd, int numberBooks, string city, string district){
     Name = name;
     WorkHoursStart = workHoursStart;
     WorkHoursEnd = workHoursEnd;
@@ -20,6 +20,10 @@ class Library{
 
   public void PrintInfo(){
     Console.WriteLine($"Library name: {Name}\nWorking Hours: {WorkHoursStart} - {WorkHoursEnd}\nNumber of books: {NumberBooks}\nCity: {City}\nDistrict: {District}");
+  }
+  
+  public bool IsWorkingNow(){
+  
   }
 
 
