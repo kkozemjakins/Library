c2a566b6bd16e8eb38dd9550b0c0655a60bc445f
kkozemjakins <kkozemjakins@gmail.com>
2022-03-11 07:48:18 +0200

    add class function

--- Library.cs
+++ Library.cs
@@ -3,19 +3,23 @@ using System;
 class Library{
   public string Name;
   public int WorkHoursStart;
-  public int WorkHoursEnd = 20;
+  public int WorkHoursEnd;
   public int NumberBooks;
   public string City;
-  public string Rajons;
+  public string District;
 
-  public Library(string name, int workHoursStart, int numberBooks, string city, string rajons){
+  public Library(string name, int workHoursStart, int workHoursEnd, int numberBooks, string city, string district){
     Name = name;
     WorkHoursStart = workHoursStart;
-    //WorkHoursEnds = workHoursEnd;
+    WorkHoursEnd = workHoursEnd;
     NumberBooks = numberBooks;
     City = city;
-    Rajons = rajons;
+    District = district;
 
+  }
+
+  public void PrintInfo(){
+    Console.WriteLine($"Library name: {Name}\nWorking Hours: {WorkHoursStart} - {WorkHoursEnd}\nNumber of books: {NumberBooks}\nCity: {City}\nDistrict: {District}");
   }
 
 
--- Program.cs
+++ Program.cs
@@ -3,11 +3,9 @@
 class Program 
 {
   public static void Main (string[] args) {
-    Library Test = new Library("Rīgas Centrālās bibliotēka",12,2000,"Rīga", "Imanta");
+    Library Test = new Library("Rīgas Centrālās bibliotēka",12,20,2000,"Rīga", "Imanta");
 
-    Console.WriteLine($"Strādnieka Vārds: {Test.Name}, darba laiks: {Test.WorkHoursStart}-{Test.WorkHoursEnd} stundas, profesija: {Test.NumberBooks}, City: {Test.City}, Rajons: {Test.Rajons}");
-
-    Console.WriteLine(Test.IsWorkingNow);
+    Test.PrintInfo();
   }
 
 }
